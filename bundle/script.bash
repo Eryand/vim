550fe36cbbaaac3f2e1b49a4784922fc6610f85c
Eryand <christian.milan1@free.fr>
2017-10-17 17:30:01 +0200

    Fin des plugins

--- bundle/script.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone https://github.com/vim-syntastic/syntastic
-git clone https://vimawesome.com/plugin/editorconfig-vim
-git clone https://github.com/tpope/vim-fugitive
-
--- vimrc
+++ vimrc
@@ -1,6 +1,6 @@
 execute pathogen#infect()
-
 set nocompatible
 set number
+set t_Co=256
 syntax on
 filetype plugin indent on
